ac1116ca2bf0da43d6008b809468cf790078e8d6
quan525 <quanchu64@gmail.com>
2024-06-04 22:33:54 +0700

    Link postgres

--- config/postgresdb.js
+++ config/postgresdb.js
@@ -1,11 +1,21 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "minjeong12",
-    host: "localhost",
-    port: 5432,
-    database: "Social Media Website"
-})
+    connectionString: process.env.DBConfigLink,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "minjeong12",
+//     host: "localhost",
+//     port: 5432,
+//     database: "Social Media Website"
+// })
 
 module.exports = pool
