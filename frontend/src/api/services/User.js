727364955abc04d434067f130aaac0df832501c1
quan525 <quanchu64@gmail.com>
2024-06-11 12:47:25 +0700

    Update api function

--- frontend/src/api/services/User.js
+++ frontend/src/api/services/User.js
@@ -61,7 +61,7 @@ export const ForgotPassword = async (username, webRootUrl) => {
     return result
 }
 
-export const PasswordReset = async (userId, resetPassword, token) => {
+export const ResetPassword = async (userId, resetPassword, token) => {
     const result = await axios.post(`${API_PATHS.api}/auth/reset_password`, {userId, resetPassword, token})
     return result
 }
